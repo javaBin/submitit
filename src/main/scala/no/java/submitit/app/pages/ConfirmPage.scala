d0543423abb978e745781aa32be5ebdbb047a23f
Alf Kristian Stoyle <alf.kristian@gmail.com>
2008-11-26 20:34:16 +0100

    Dynamically generate full url for id resolve page.

--- src/main/scala/no/java/submitit/app/pages/ConfirmPage.scala
+++ src/main/scala/no/java/submitit/app/pages/ConfirmPage.scala
@@ -5,9 +5,15 @@ import no.java.submitit.model._
 import no.java.submitit.app.State
 import org.apache.wicket.markup.html.basic._
 import org.apache.wicket.markup.html.link._
+import org.apache.wicket.protocol.http.servlet.ServletWebRequest
+import javax.servlet.http.HttpServletRequest
 
 class ConfirmPage extends LayoutPage {
-  
-  add(new ExternalLink("confirmUrl", "http://localhost:8040/submitit/lookupPresentation?id=1", "http://localhost:8040/submitit/lookupPresentation?id=1"))
+
+  // TODO actually post
+  val request = getRequest.asInstanceOf[ServletWebRequest].getHttpServletRequest
+  val url = "http://" + request.getServerName + ":" + request.getServerPort + request.getContextPath + "/"
+  val uniqueId = 10
+  add(new ExternalLink("confirmUrl", url + "lookupPresentation?id=" + uniqueId, url + "lookupPresentation?id=" + uniqueId))
 
 }
