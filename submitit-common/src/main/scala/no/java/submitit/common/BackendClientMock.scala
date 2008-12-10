7b71ce8d093b315116a2efea9ccb2714645a09d9
Fredrik Vraalsen <fredriv@badwolf.(none)>
2008-12-10 12:17:33 +0100

    added mock in-memory implementation of BackendClient

---
+++ submitit-common/src/main/scala/no/java/submitit/common/BackendClientMock.scala
@@ -0,0 +1,24 @@
+package no.java.submitit.common
+
+import scala.collection.mutable.Map
+
+import no.java.submitit.model.Presentation
+
+class BackendClientMock extends BackendClient {
+
+  var nextId = 1
+  
+  val presentations = Map.empty[String, Presentation]
+  
+  def savePresentation(pres: Presentation): String = {
+    pres.sessionId = nextId.toString
+    nextId = nextId + 1
+    presentations(pres.sessionId) = pres
+    pres.sessionId
+  }
+  
+  def loadPresentation(id: String): Presentation = {
+    presentations(id)
+  }
+  
+}
