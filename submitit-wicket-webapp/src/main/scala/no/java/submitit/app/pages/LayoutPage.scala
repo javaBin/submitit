dc9f975d93b80150227a324b1b10d391635f6a75
Alf Kristian Stoyle <alf.kristian@gmail.com>
2008-12-22 17:14:38 +0100

    Added property for header text. Also removed link and set up h1 style.

--- submitit-wicket-webapp/src/main/scala/no/java/submitit/app/pages/LayoutPage.scala
+++ submitit-wicket-webapp/src/main/scala/no/java/submitit/app/pages/LayoutPage.scala
@@ -1,7 +1,10 @@
 package no.java.submitit.app.pages
 
 import org.apache.wicket.markup.html.WebPage
+import org.apache.wicket.markup.html.basic.Label
 
 class LayoutPage extends WebPage {
+  
+  add(new Label("headerText", SubmititApp.getSetting("headerText")))
 
 }
