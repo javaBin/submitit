b949acfb5f0189b1a26b5446269453819b89b228
Fredrik Vraalsen <fredriv@badwolf.(none)>
2009-01-25 15:29:26 +0100

    fix serialization

--- submitit-wicket-webapp/src/main/scala/no/java/submitit/app/pages/LazyPageLink.scala
+++ submitit-wicket-webapp/src/main/scala/no/java/submitit/app/pages/LazyPageLink.scala
@@ -1,9 +1,10 @@
 package no.java.submitit.app.pages
 
+import _root_.java.io.Serializable
 import org.apache.wicket.Page
 import org.apache.wicket.markup.html.link.IPageLink
 
-class LazyPageLink[C <: Page](f: => Page, c: Class[C]) extends IPageLink {
+class LazyPageLink[C <: Page](f: => Page, c: Class[C]) extends IPageLink with Serializable {
   
   def getPage = f
   
