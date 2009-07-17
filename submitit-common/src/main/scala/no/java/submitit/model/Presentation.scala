a1bfe7723a8882464764fa3373856d3211bd2322
Alf Kristian Stoyle <alf.kristian@gmail.com>
2009-07-17 13:41:26 +0200

    Added keywords to confirmation page and email

--- submitit-common/src/main/scala/no/java/submitit/model/Presentation.scala
+++ submitit-common/src/main/scala/no/java/submitit/model/Presentation.scala
@@ -78,6 +78,7 @@ class Presentation extends Serializable {
     "\nLevel: " + level +
     "\nEquipment:\n" + toNonNullString(equipment) +
     "\n\nExpected audience:\n" + toNonNullString(expectedAudience) +
+    "\n\nTags:\n" + keywords.mkString(", ") +
     "\n\nSpeakers:\n\n" + speakers.mkString("", "\n\n", "")
 
   def toNonNullString(s: String) = if (s != null) s else ""
