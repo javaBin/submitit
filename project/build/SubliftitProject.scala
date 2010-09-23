33e6303590061c80bc735825befdf20451ab7f8b
Alf Kristian Stoyle <alf.kristian@gmail.com>
2010-09-23 20:03:00 +0200

    Intermediate project definition for subliftit/submitit.

--- project/build/SubliftitProject.scala
+++ project/build/SubliftitProject.scala
@@ -1,13 +1,65 @@
 import sbt._
+import java.io.File       
+import sbt.BasicScalaProject._
 
+class SubmititProject(info: ProjectInfo) extends ParentProject(info){
+  
+  val javabin_release = "javaBin Smia" at "http://smia.java.no/maven/repo/release"	
+  //Necessary for transitive dependencies in ems-client org.restlet. Project won't build without it
+  val maven_restlet = "Public online Restlet repository" at "http://maven.restlet.org"
 
-class SubliftitProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val lift_webkit = "net.liftweb" %% "lift-webkit" % "2.1-RC1"
-  val lift_mapper = "net.liftweb" %% "lift-mapper" % "2.1-RC1"
-  val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.21" % "test"
-  val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided"
+  val ems_version = "1.1"
+  val wicket_version = "1.4.10"
+  val junit_version = "4.5"
 
-  override def scanDirectories = Nil
+  override def parallelExecution = true
 
-  override def jettyWebappPath = webappPath
+  override def disableCrossPaths = true
+
+  trait OutPutPaths extends BasicScalaProject {
+
+	override def outputPath = "target"
+    override def mainCompilePath = outputPath / "classes"
+	override def testCompilePath = outputPath / "test-classes"
+	override val mainResourcesOutputPath = "target" / "classes"
+    override val testResourcesOutputPath = "target" / "test-classes"
+
+    protected override def copyResourcesAction = copyTask(mainResources, mainResourcesOutputPath) describedAs CopyResourcesDescription
+    protected override def copyTestResourcesAction = copyTask(testResources, testResourcesOutputPath) describedAs CopyTestResourcesDescription
+
+  }
+
+  lazy val common = project("submitit-common", "Common", new DefaultProject(_) with OutPutPaths {
+    val slf4j = "org.slf4j" % "slf4j-log4j12" % "1.4.2"
+    val log4j = "log4j" % "log4j" % "1.2.14"
+  })
+  
+  lazy val ems = project("submitit-ems-client", "Ems Client", new DefaultProject(_) with OutPutPaths {
+    val ems_client = "no.java.ems" % "ems-client" % ems_version
+    val junit = "junit" % "junit" % "4.5" % "test"    	
+  }, common)
+  
+  lazy val ui = project("submitit-webapp", "WebApplication", new DefaultWebProject(_) with OutPutPaths {
+    system[File]("submitit.properties")() = "src" / "test" / "resources" / "submitit.properties" asFile
+    override def mainResources = super.mainResources +++ descendents( mainSourceRoots, "*" ) --- mainSources
+	
+    val ems_wiki = "no.java.ems" % "ems-wiki" % ems_version
+    val wicket = "org.apache.wicket" % "wicket" % wicket_version
+    val wicket_extensions = "org.apache.wicket" % "wicket-extensions" % wicket_version
+    val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided"
+    val mail = "javax.mail" % "mail" % "1.4.1"
+    val jetty_server = "org.mortbay.jetty" % "jetty" % "6.1.14" % "test"
+  }, common, ems)
+  
+  lazy val lift = project("subliftit", "Lift webapp", new DefaultWebProject(_) with OutPutPaths {
+    val lift_webkit = "net.liftweb" %% "lift-webkit" % "2.1-RC1"
+    val lift_mapper = "net.liftweb" %% "lift-mapper" % "2.1-RC1"
+    val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.21" % "test"
+    val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided"
+
+    override def scanDirectories = Nil
+
+    override def jettyWebappPath = webappPath
+  }, common, ems)
+  
 }
