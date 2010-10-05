e4ce9a64dbd27955090c922d41260b4f30b062ac
Alf Kristian Stoyle <alf.kristian@gmail.com>
2010-10-05 22:27:21 +0200

    A little better api on mock!? And a little more testing.

--- subliftit/src/test/scala/subliftit/config/ConfigMock.scala
+++ subliftit/src/test/scala/subliftit/config/ConfigMock.scala
@@ -4,22 +4,34 @@ import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
 import no.java.submitit.config.{Keys, ConfigKey}
-import no.java.submitit.config.Keys.eventName
+import no.java.submitit.config.Keys.{submititBaseUrl, eventName}
 
 trait ConfigMock extends Config {
-  def fromKey: PartialFunction[ConfigKey, Option[String]]
+  def setup: PartialFunction[ConfigKey, Option[String]]
 
-  override def configValue(key: ConfigKey): Option[String] = fromKey(key)
+  override def configValue(key: ConfigKey): Option[String] = setup(key)
 
 }
 
 @RunWith(classOf[JUnitRunner])
 class ConfigMockTest extends FunSuite {
 
-   test("That configmock works as expected and check out the api") {
+   test("That configmock works as expected and check what the mock api looks like") {
+
+     intercept[Exception] {
+       new Config{}.configValue(eventName)
+     }
+
+     intercept[MatchError] {
+       val config = new Config with ConfigMock {
+         def setup = {case `eventName` => None}
+       }
+       config.configValue(submititBaseUrl)
+     }
+
      val res = Some("an event")
      object myTestClass extends Config with ConfigMock {
-       def fromKey = {case _ => res}
+       def setup = {case `eventName` => res}
      }
      assert(res === myTestClass.configValue(eventName))
    }
