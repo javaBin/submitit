5b36e4838a8b110c9081fa97593a6c34d8aaa8ec
Alf Kristian Stoyle <alf.kristian@gmail.com>
2010-12-23 13:46:45 +0100

    Updated to latest version of junit.

--- project/build/SubmititProject.scala
+++ project/build/SubmititProject.scala
@@ -28,7 +28,7 @@ class SubmititProject(info: ProjectInfo) extends ParentProject(info){
   lazy val common = project("submitit-common", "Common", new DefaultProject(_) with OutPutPaths {
     val slf4j = "org.slf4j" % "slf4j-log4j12" % "1.4.2"
     val log4j = "log4j" % "log4j" % "1.2.14"
-    val junit = "junit" % "junit" % "4.5" % "test"
+    val junit = "junit" % "junit" % "4.8.2" % "test"
     val scala_test = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final-SNAPSHOT" % "test"
   })
   
